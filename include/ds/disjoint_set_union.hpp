0c58d6689cf712d98f97aea2c4c5084bb9d108f7
Stefano Bergamaschi <stefano.bergamaschi.2010@outlook.it>
2026-05-26 18:50:38 +0200

    Bug Fixing

--- include/ds/disjoint_set_union.hpp
+++ include/ds/disjoint_set_union.hpp
@@ -1,8 +1,9 @@
 #ifndef disjoint_set_union
 #define disjoint_set_union
 
-#include <vector>
+#include <stdexcept>
 #include <unordered_map>
+#include <vector>
 
 namespace ds{
     template<typename T>
@@ -18,7 +19,7 @@ namespace ds{
             // * @brief Finds the root of the set containing the element at the given index, applying path compression.
             // * @param The index of the element.
             // * @return The index of the root of the set containing the element.
-            size_t find_root_by_index(size_t index) const {
+            size_t find_root_by_index(size_t index){
                 size_t root = index;
                 while(root != parent_[root]) root = parent_[root];
 
@@ -36,8 +37,7 @@ namespace ds{
             // * @param The element to convert.
             // * @return The index corresponding to the element.
             size_t convert_to_index(T element){
-                return element_to_id_.at(element);
-                //return element_to_id_[element];
+                return element_to_id_[element];
             }
 
             
@@ -71,8 +71,7 @@ namespace ds{
             // * @param The second element.
             // * @return True if the elements were in different sets and were united, false otherwise.
             bool unite(T element1, T element2){
-                add_element(element1);
-                add_element(element2);
+                if(!contains(element1) or !contains(element2)) return false;
 
                 size_t index1 = find_root_by_index(convert_to_index(element1));
                 size_t index2 = find_root_by_index(convert_to_index(element2));
@@ -96,25 +95,10 @@ namespace ds{
             // * @param The first element.
             // * @param The second element.
             // * @return True if the elements were in different sets and were united, false otherwise.
+            // * @throws runtime_error if either element is not in the DSU.
             bool strict_unite(T element1, T element2){
-                if(!contains(element1) or !contains(element2)) return false;
-                
-                size_t index1 = find_root_by_index(convert_to_index(element1));
-                size_t index2 = find_root_by_index(convert_to_index(element2));
-
-                if(index1 == index2) return false;
-
-                if(size_[index1] < size_[index2]){
-                    parent_[index1] = index2;
-                    size_[index2] += size_[index1];
-                }  
-                else{
-                    parent_[index2] = index1;
-                    size_[index1] += size_[index2];
-                } 
-                set_count_--;
-                
-                return true;
+                if(!contains(element1) or !contains(element2)) throw std::runtime_error("Element not in DSU");
+                return unite(element1, element2);
             }
 
             // * @brief Checks if two elements are in the same set.
@@ -122,7 +106,7 @@ namespace ds{
             // * @param The second element.
             // * @return True if the elements are in the same set, false otherwise.
             bool same_set(T element1, T element2){
-                if(add_element(element1) or add_element(element2)) return false;
+                if(!contains(element1) or !contains(element2)) return false;
                 if(find_root_by_index(convert_to_index(element1)) == find_root_by_index(convert_to_index(element2))) return true;
                 return false;
             }
@@ -131,10 +115,10 @@ namespace ds{
             // * @param The first element.
             // * @param The second element.
             // * @return True if the elements are in the same set, false otherwise.
+            // * @throws runtime_error if either element is not in the DSU.
             bool strict_same_set(T element1, T element2){
-                if(!contains(element1) or !contains(element2)) return false;
-                if(find_root_by_index(convert_to_index(element1)) == find_root_by_index(convert_to_index(element2))) return true;
-                return false;
+                if(!contains(element1) or !contains(element2)) throw std::runtime_error("Element not in DSU");
+                return same_set(element1, element2);
             }
 
             // * @brief Returns the number of elements in the DSU.
@@ -147,17 +131,9 @@ namespace ds{
             // * @param The element.
             // * @return The size of the set containing the element.
             size_t set_size(T element){
-                if(add_element(element)) return 1;
-                return size_[find_root_by_index(convert_to_index(element))];
-            }
-
-            // * @brief Returns the size of the set containing the element only if the element is already in the DSU.
-            // * @param The element.
-            // * @return The size of the set containing the element, or 0 if the element is not in the DSU.
-            size_t strict_set_size(T element){
                 if(!contains(element)) return 0;
                 return size_[find_root_by_index(convert_to_index(element))];
-            }   
+            }
 
             // * @brief Returns the number of sets in the DSU.
             // * @return The number of sets in the DSU.
@@ -169,19 +145,6 @@ namespace ds{
             // * @param The element.
             // * @return A vector containing all elements in the set.
             std::vector<T> get_set(T element){
-                if(add_element(element)) return vector<T>{element};
-                std::vector<T> result;
-                size_t root_index = find_root_by_index(convert_to_index(element));
-                for(size_t i = 0; i < next_index_; i++){
-                    if(find_root_by_index(i) == root_index) result.push_back(id_to_element_[i]);
-                }
-                return result;
-            }
-
-            // * @brief Returns all elements in the set containing the element only if the element is already in the DSU.
-            // * @param The element.
-            // * @return A vector containing all elements in the set, or an empty vector if the element is not in the DSU.
-            std::vector<T> strict_same_set(T element){
                 if(!contains(element)) return std::vector<T>{};
                 std::vector<T> result;
                 size_t root_index = find_root_by_index(convert_to_index(element));
@@ -218,6 +181,14 @@ namespace ds{
                 id_to_element_.clear();
                 next_index_ = 0;
                 set_count_ = 0;
+            }
+
+            // * @brief Reserves space for a given number of elements in the DSU to optimize memory usage and performance.
+            // * @param size The number of elements to reserve space for.
+            void reserve(size_t size){
+                parent_.reserve(size);
+                size_.reserve(size);
+                id_to_element_.reserve(size);
             }
     };
 }
